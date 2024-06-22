34cd168e68e58407f0033c9330582f16304f7eb7
Ronald Mindiola <ronaldmindiola@live.com>
2024-06-21 22:58:24 -0500

    Integraciones MDX, React. React, React DOM, Netlify. Script dev. TS config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ...
+  integrations: [
+    mdx(),
+    react({
+      include: ["**/react/*"],
+    }),
+  ],
+});
